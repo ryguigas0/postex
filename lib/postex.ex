d4af1daac19846f352ce8c19621adfb502913aac
ryguigas0 <guigacasa@protonmail.ch>
2021-09-21 19:34:30 -0300

    Made post controller router shorter and manageable

--- lib/postex.ex
+++ lib/postex.ex
@@ -6,4 +6,11 @@ defmodule Postex do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  alias Postex.Posts.{Create, Delete, Edit, Update, Show}
+
+  defdelegate create_post(params), to: Create, as: :call
+  defdelegate show_post(post_id), to: Show, as: :call
+  defdelegate delete_post(post_id), to: Delete, as: :call
+  defdelegate update_post(post_id, updates), to: Update, as: :call
+  defdelegate edit_post(post_id, edits), to: Edit, as: :call
 end
--- lib/postex_web/controllers/post_controller.ex
+++ lib/postex_web/controllers/post_controller.ex
@@ -4,51 +4,43 @@ defmodule PostexWeb.PostsController do
   action_fallback PostexWeb.PostFallbackController
 
   alias PostexWeb.PostView
-  alias Postex.{Post, Posts}
+  alias Postex.Post
 
   def create(conn, %{"content" => content}) do
     with {:ok, post} <-
-           Posts.Create.call(%Post{content: content, likes: 0, shares: 0, edited: false}) do
-      conn
-      |> put_status(201)
-      |> put_view(PostView)
-      |> render("create.json", post: post)
+           Postex.create_post(%Post{content: content, likes: 0, shares: 0, edited: false}) do
+      response(conn, 201, "create.json", post: post)
     end
   end
 
   def show(conn, %{"id" => post_id}) do
-    with {:ok, post} <- Posts.Show.call(post_id) do
-      conn
-      |> put_status(200)
-      |> put_view(PostView)
-      |> render("show.json", post: post)
+    with {:ok, post} <- Postex.show_post(post_id) do
+      response(conn, 200, "show.json", post: post)
     end
   end
 
   def delete(conn, %{"id" => post_id}) do
-    with {:ok, _deleted_post} <- Posts.Delete.call(post_id) do
-      conn
-      |> put_status(200)
-      |> put_view(PostView)
-      |> render("delete.json")
+    with {:ok, _deleted_post} <- Postex.delete_post(post_id) do
+      response(conn, 201, "delete.json")
     end
   end
 
   def update(conn, %{"id" => post_id, "likes" => new_likes, "shares" => new_shares}) do
-    with {:ok, updated_post} <- Posts.Update.call(post_id, likes: new_likes, shares: new_shares) do
-      conn
-      |> put_status(200)
-      |> put_view(PostView)
-      |> render("show.json", post: updated_post)
+    with {:ok, updated_post} <- Postex.update_post(post_id, likes: new_likes, shares: new_shares) do
+      response(conn, 200, "show.json", post: updated_post)
     end
   end
 
   def edit(conn, %{"id" => post_id, "content" => new_content}) do
-    with {:ok, updated_post} <- Posts.Edit.call(post_id, content: new_content) do
-      conn
-      |> put_status(200)
-      |> put_view(PostView)
-      |> render("show.json", post: updated_post)
+    with {:ok, edited_post} <- Postex.edit_post(post_id, content: new_content) do
+      response(conn, 201, "show.json", post: edited_post)
     end
+  end
+
+  defp response(conn, status, template, args \\ []) do
+    conn
+    |> put_status(status)
+    |> put_view(PostView)
+    |> render(template, args)
   end
 end
